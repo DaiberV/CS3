4c8fb2b6ea20ff9088342eb148da2624a0f2bcb4
DaiberV <vergeldaiber@americana.edu.co>
2017-03-08 17:31:59 -0500

    Actualizacion proyecto y webservices

--- SQLComparacion/01 - Diferencias entre DBs SQLServer.sql
+++ SQLComparacion/01 - Diferencias entre DBs SQLServer.sql
@@ -97,19 +97,59 @@ LEFT JOIN <<DESTINO>>.sys.Columns as fcol On fin.object_id=fcol.object_id and ic
 WHERE fcol.name is null;
 
 -- Modifica Atributos
-SELECT 'ALTER TABLE '+ini.name+' ALTER COLUMN '+icol.name+' '+type_name(icol.system_type_id)+
-	(CASE 
-		WHEN type_name(icol.system_type_id) = 'decimal' and icol.max_length = 9
-			THEN '(15,2)'
-		WHEN type_name(icol.system_type_id) in ('datetime','bit','int','smallint','money','smallmoney')
-			THEN ''
-		WHEN type_name(icol.system_type_id) in ('varchar','varbinary')
-			THEN '(max)'
+WITH [TT] AS(SELECT ini.name, icol.name AS fname, type_name(icol.system_type_id) AS [TIPO ORIGEN],TYPE_NAME(fcol.system_type_id) AS [TIPO DESTINO]
+	,CASE
+		WHEN TYPE_NAME(icol.system_type_id) = 'decimal' and icol.max_length = 9
+			THEN CONVERT(DECIMAL(4,1),(CONVERT(decimal,(RTRIM(convert(char,icol.precision)) + '' + RTRIM(convert(char,icol.scale))))/10))
 		ELSE
-			'('+CONVERT(VARCHAR(4),icol.max_length)+')'
-	END)+' not null;'
+			icol.max_length
+		END	as [prueba]		
+	,CASE
+		WHEN TYPE_NAME(fcol.system_type_id) = 'decimal' and fcol.max_length = 9
+			THEN CONVERT(DECIMAL(4,1),(CONVERT(decimal,(RTRIM(convert(char,fcol.precision)) + '' + RTRIM(convert(char,fcol.scale))))/10))
+		ELSE
+			fcol.max_length
+		END	as [prueba2]
+	,CASE
+		WHEN icol.is_nullable = 1
+			THEN 'NULL'
+		ELSE
+			'NOT NULL'
+		END AS [ISNULL]
+	,CASE
+		WHEN fcol.is_nullable = 1
+			THEN 'NULL'
+		ELSE
+			'NOT NULL'
+		END AS [FISNULL]
 FROM <<ORIGEN>>.sys.Tables as ini
 INNER JOIN <<ORIGEN>>.sys.Columns as icol On ini.object_id=icol.object_id
 INNER JOIN <<DESTINO>>.sys.Tables as Fin On ini.name=fin.name
 INNER JOIN <<DESTINO>>.sys.Columns as fcol On fin.object_id=fcol.object_id and icol.name=fcol.name
-WHERE icol.system_type_id<>fcol.system_type_id or icol.max_length<>fcol.max_length;
+)
+, 
+[GG] as(
+SELECT
+	TT.name					AS	[BASE ORIGEN]
+	,TT.fname				AS	[BASE DESTINO]
+	,TT.[TIPO ORIGEN]		AS	[TIPO ORIGEN]
+	,TT.prueba				AS	[TAMAÑO ORIGEN]
+	,TT.ISNULL				AS  [VACIO ORIGEN]
+	,TT.[TIPO DESTINO]		AS	[TIPO DESTINO]
+	,TT.prueba2				AS	[TAMAÑO DESTINO]
+	,TT.FISNULL				AS	[VACIO DESTINO]
+FROM
+	TT
+WHERE
+	TT.prueba <> TT.prueba2
+	
+)
+SELECT
+case
+	WHEN gg.[TIPO ORIGEN] <> 'decimal'
+		Then 'ALTER TABLE '+GG.[BASE ORIGEN]+' ALTER COLUMN '+GG.[BASE DESTINO]+' '+GG.[TIPO ORIGEN]+' ('+REPLACE(RTRIM(convert(CHAR,(GG.[TAMAÑO ORIGEN]))),'.0','')+') '+GG.[VACIO ORIGEN]  
+	ELSE
+		'ALTER TABLE '+gg.[BASE ORIGEN]+' ALTER COLUMN '+gg.[BASE DESTINO]+' '+gg.[TIPO ORIGEN]+' ('+REPLACE(RTRIM(convert(char,gg.[TAMAÑO ORIGEN])),'.',',')+') '+gg.[VACIO ORIGEN]
+	END
+From
+	gg;
